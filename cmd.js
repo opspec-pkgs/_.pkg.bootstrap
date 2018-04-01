21bc5940f432d839907bc31825e29f1136fa7c3d
Chris Dostert <chris.dostert@live.com>
2018-03-31 19:02:03 -0700

    chore) use object initializer syntax

--- cmd.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const opYml = require('/opYml.json');
-const contentList = require('/opContentList.json');
-
-fs.writeFileSync('/data', JSON.stringify({opYml, contentList}))

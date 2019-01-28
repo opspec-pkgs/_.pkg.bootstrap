700ac073f925cb3c17194268a2947548873708c8
Chris Dostert <chris.dostert@live.com>
2019-01-28 12:02:22 -0800

    feat) defaults for optional inputs fix) booleans duplicated

--- helpers/ifAnyRequiredInputs.js
+++ helpers/ifAnyRequiredInputs.js
@@ -1,9 +1,9 @@
 const _isInputRequired = require('./_isInputRequired')
 
 module.exports = function (inputs, options) {
-    const isAnyOptionalInputs = Object.values(inputs||{}).find(input => !_isInputRequired(input));
+    const isAnyRequiredInputs = Object.values(inputs||{}).find(input => _isInputRequired(input));
 
-    if (isAnyOptionalInputs) {
+    if (isAnyRequiredInputs) {
         return options.fn(this);
     }
 };
--- helpers/ifOpInputRequired.js
+++ helpers/ifOpInputRequired.js
@@ -1,15 +1,7 @@
-module.exports = function (input, options) {
-    let isRequired;
-    if (input.array) isRequired = !input.array.default;
-    if (input.boolean) isRequired = input.boolean.default !== undefined;
-    if (input.dir) isRequired = !input.dir.default;
-    if (input.file) isRequired = !input.file.default;
-    if (input.number) isRequired = !input.number.default;
-    if (input.object) isRequired = !input.object.default;
-    if (input.socket) isRequired = !input.socket.default;
-    if (input.string) isRequired = !input.string.default;
+const _isInputRequired = require('./_isInputRequired')
 
-    if (isRequired) {
+module.exports = function (input, options) {
+    if (_isInputRequired(input)) {
         return options.fn(this);
     }
 };
